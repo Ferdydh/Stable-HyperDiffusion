bcff515aafbac8cc19cba9616b0281a126374734
Ferdy Dermawan Hadiwijaya <ferdydh.hadiwijaya@tum.de>
2024-12-06 18:05:42 +0100

    unfinished diffusion

--- lhdm/main.py
+++ lhdm/main.py
@@ -1,9 +1,11 @@
 import typer
 import core.train
+import core.train_diffusion
 import core.visualize
 
 app = typer.Typer(pretty_exceptions_show_locals=False)
 app.command()(core.train.train)
+app.command()(core.train_diffusion.train_diffusion)
 app.command()(core.visualize.visualize)
 
 if __name__ == "__main__":
